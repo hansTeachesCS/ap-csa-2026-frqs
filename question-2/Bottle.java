c5c16f3c93bc0f40f789201e8575d9f55c176302
Joshua Hans <152180694+hansTeachesCS@users.noreply.github.com>
2026-05-21 09:57:31 -0400

    Implement updateAmount method in Bottle class
    Add updateAmount method to manage current amount in the bottle.

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,5 +1,17 @@
 public class Bottle {
+    private double capacity;
+    private double currentAmount;
 
+    public Bottle(double c) {
+        capacity = c;
+        currentAmount = c;
+    }
 
-
+    public double updateAmount(double amount) {
+        currentAmount -= amount;
+        if (currentAmount < 0.25*capacity) {
+            currentAmount = capacity;
+        }
+        return currentAmount;
+    }
 }
